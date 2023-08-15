39cec3b4cd4b7b6c4ce8169051a02d4af36479fc
atonya-bravin <bravinatonya.brani@gmail.com>
2023-08-15 19:03:31 +0300

    Application entry point

---
+++ TypingGame/app.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypingGame
+{
+    class app
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
