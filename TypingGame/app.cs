3600eafca94f71721f49262b4afb932ab57ccf71
atonya-bravin <bravinatonya.brani@gmail.com>
2023-08-16 23:45:39 +0300

    Typing app with the correct and wrong word checker, including the console color change feature

--- TypingGame/app.cs
+++ TypingGame/app.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TypingGame
 {
     class app
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int score = 0;
+            int wrong_spelling = 0;
+            int numberOfTries = 0;
+            while (true){
+                using (HttpClient client = new HttpClient())
+                {
+                    string randomWordApiUrl = "https://random-word-api.herokuapp.com/word";
+                    HttpResponseMessage response = await client.GetAsync(randomWordApiUrl);
+
+                    if (response.IsSuccessStatusCode){
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        string randomWord = responseBody.Replace("[\"", "").Replace("\"]", "");
+                        Console.WriteLine("Type exit() to Exit");
+                        Console.WriteLine($"Random Word ({randomWord}): ");
+                        string TypedWord = Console.ReadLine();
+                        if (TypedWord == randomWord){
+                            score++;
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Right!!\n");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            continue;
+                        }
+                        else if(TypedWord == "exit()"){
+                            Console.WriteLine("Good Bye!!\n\n");
+                            break;
+                        }
+                        else{
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Wrong!!\n");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            wrong_spelling++;
+                            continue;
+                        }
+                    }
+                    else{
+                        Console.WriteLine("Failed to fetch a random word.");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            numberOfTries = score + wrong_spelling;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"You have gotten {score} / {numberOfTries} right!");
+            Console.WriteLine("Thank you for participating\n");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
